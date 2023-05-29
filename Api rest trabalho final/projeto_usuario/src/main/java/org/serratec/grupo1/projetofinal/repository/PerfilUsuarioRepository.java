4ff1b35dab1503d04d9df047b0f6a9fe762917fb
Pedro Oliveira <pedro.oliveira0022@gmail.com>
2023-05-28 22:55:06 -0300

    Terminando o tratamento de erros no service e adicionando buscas no repository

--- Api rest trabalho final/projeto_usuario/src/main/java/org/serratec/grupo1/projetofinal/repository/PerfilUsuarioRepository.java
+++ Api rest trabalho final/projeto_usuario/src/main/java/org/serratec/grupo1/projetofinal/repository/PerfilUsuarioRepository.java
@@ -2,7 +2,11 @@ package org.serratec.grupo1.projetofinal.repository;
 
 import org.serratec.grupo1.projetofinal.model.PerfilUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Query;
 
-public interface PerfilUsuarioRepository extends JpaRepository<PerfilUsuario,Long>{
-   
+public interface PerfilUsuarioRepository extends JpaRepository<PerfilUsuario, Long> {
+    @Query(value = "SELECT pu  FROM PerfilUsuario pu JOIN pu.usuario u where u.id = ?1")
+    PerfilUsuario findByIdUsuario(Long idUsuario);
 }
--- Api rest trabalho final/projeto_usuario/src/main/java/org/serratec/grupo1/projetofinal/service/PerfilUsuarioService.java
+++ Api rest trabalho final/projeto_usuario/src/main/java/org/serratec/grupo1/projetofinal/service/PerfilUsuarioService.java
@@ -1,6 +1,5 @@
 package org.serratec.grupo1.projetofinal.service;
 
-	
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,88 +14,79 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PerfilUsuarioService implements ICRUDService<PerfilUsuarioRequestDto, PerfilUsuarioResponseDto> { 
-	
-		
+public class PerfilUsuarioService implements ICRUDService<PerfilUsuarioRequestDto, PerfilUsuarioResponseDto> {
+
 	@Autowired
 	private PerfilUsuarioRepository perfilUsuarioRepository;
 
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@Autowired
 	private ModelMapper mapper;
 
 	@Override
 	public List<PerfilUsuarioResponseDto> obterTodos() {
 		return perfilUsuarioRepository.findAll().stream()
-		.map(p -> mapper.map(p, PerfilUsuarioResponseDto.class))
-		.collect(Collectors.toList());
+				.map(p -> mapper.map(p, PerfilUsuarioResponseDto.class))
+				.collect(Collectors.toList());
 	}
-		/* 
-	List<PerfilUsuario> perfilUsuarios = perfilUsuarioRepository.findAll();
-	
-	List<PerfilUsuarioResponseDto> perfilUsuariosDto = new ArrayList<>();
-	
-	for(PerfilUsuario perfilUsuario: perfilUsuarios) {
-		var dto = mapper.map(perfilUsuario,PerfilUsuarioResponseDto.class);	
-		perfilUsuariosDto.add(dto);
-		}
-		return perfilUsuariosDto;
-	}
-	*/
+
 	@Override
 	public PerfilUsuarioResponseDto obterPorId(Long id) {
 		return perfilUsuarioRepository.findById(id)
-		.map(p -> mapper.map(p, PerfilUsuarioResponseDto.class))
-		.orElseThrow(() -> new ResourceNotFoundException("Vovó delaide!"));
+				.map(p -> mapper.map(p, PerfilUsuarioResponseDto.class))
+				.orElseThrow(() -> new ResourceNotFoundException("Perfil usuário não encontrado"));
 	}
 
-		/* 
-    Optional<PerfilUsuario> optPerfilUsuario = perfilUsuarioRepository.findById(id);
-	if (optPerfilUsuario.isEmpty()) {	
-		throw new ResourceNotFoundException("PerfilUsuario nao encontrada" + id);
-		}	
-		return mapper.map(optPerfilUsuario.get(), PerfilUsuarioResponseDto.class);
+	public void validacaoPerfilUsuario(Long idUsuario) {
+		PerfilUsuario validUsuario = perfilUsuarioRepository.findByIdUsuario(idUsuario);
+		if (validUsuario != null) {
+			throw new ResourceBadRequestException("Usuário já cadastrado!");
 		}
-		*/
-
+	}
 
 	@Override
 	public PerfilUsuarioResponseDto cadastrar(PerfilUsuarioRequestDto dto) {
+		validacaoPerfilUsuario(dto.getIdUsuario());
+		PerfilUsuario perfilUsuario = mapper.map(dto, PerfilUsuario.class);
 
-	PerfilUsuario perfilUsuario = mapper.map(dto, PerfilUsuario.class);
-	
-	perfilUsuario.setId(null);
-	perfilUsuario.setUsuario(usuarioService.findByIdModelUsuario(dto.getIdUsuario()));
-	if (perfilUsuario.getNomeCompleto() == "" || perfilUsuario.getDataNasc() == null || perfilUsuario.getGenero() == "" || perfilUsuario.getUsuario() == null) {
-		throw new ResourceBadRequestException("Favor preencher todos os campos!");
-	} else {
-		perfilUsuario = perfilUsuarioRepository.save(perfilUsuario);
-	}
-	return mapper.map(perfilUsuario, PerfilUsuarioResponseDto.class);
-	
+		perfilUsuario.setId(null);
+		perfilUsuario.setUsuario(usuarioService.findByIdModelUsuario(dto.getIdUsuario()));
+		if (perfilUsuario.getNomeCompleto().isEmpty() || perfilUsuario.getDataNasc() == null
+				|| perfilUsuario.getGenero().isEmpty() || perfilUsuario.getUsuario() == null) {
+			throw new ResourceBadRequestException("Favor preencher todos os campos!");
+		} else {
+			perfilUsuario = perfilUsuarioRepository.save(perfilUsuario);
+		}
+		return mapper.map(perfilUsuario, PerfilUsuarioResponseDto.class);
+
 	}
 
 	@Override
 	public PerfilUsuarioResponseDto atualizar(Long id, PerfilUsuarioRequestDto dto) {
-		
-	obterPorId(id);
 
-	PerfilUsuario perfilUsuario = mapper.map(dto, PerfilUsuario.class);
-	perfilUsuario.setId(id);
-	perfilUsuario = perfilUsuarioRepository.save(perfilUsuario);
-	return mapper.map(perfilUsuario, PerfilUsuarioResponseDto.class);
+		obterPorId(id);
+
+		PerfilUsuario perfilUsuario = mapper.map(dto, PerfilUsuario.class);
+		perfilUsuario.setId(id);
+		if (perfilUsuario.getNomeCompleto().isEmpty() || perfilUsuario.getDataNasc() == null
+				|| perfilUsuario.getGenero().isEmpty() || perfilUsuario.getUsuario() == null) {
+			throw new ResourceBadRequestException("Favor preencher todos os campos!");
+		} else {
+			perfilUsuarioRepository.save(perfilUsuario);
+		}
+		return mapper.map(perfilUsuario, PerfilUsuarioResponseDto.class);
+
 	}
 
-	
 	@Override
 	public void deletar(Long id) {
-	obterPorId(id);
-	
-	perfilUsuarioRepository.deleteById(id);
-		
+		perfilUsuarioRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Perfil de usuário não encontrado"));
+
+		perfilUsuarioRepository.deleteById(id);
+
 	}
 
 }
-
